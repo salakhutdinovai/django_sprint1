51c2576e1d7b8cd621be5a70696f8b636192f35e
Wxre <99921257+Wai7e@users.noreply.github.com>
2025-03-17 14:35:12 +0300

    + init structure fixed

--- blogicum/blog/blog_app/apps.py
+++ blogicum/blog/blog_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BlogicumAppConfig(AppConfig):
+class BlogAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'blogicum_app'
+    name = 'blog_app'
